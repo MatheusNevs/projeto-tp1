098301133f18b1fbf0e819555a500eeb63f2cf1a
MatheusNevs <matheusnf62@gmail.com>
2024-10-22 10:10:11 -0300

    first commit

---
+++ domains/domain.h
@@ -0,0 +1,45 @@
+#ifndef DOMAIN_INTERFACE_H
+#define DOMAIN_INTERFACE_H
+
+#include <stdexcept>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <cctype>
+#include <list>
+
+class Domain   {
+protected:
+
+    string valor;
+    virtual void validar(string entrada) = 0;
+
+public:
+    /** @brief Método Set.
+     *
+     * Método responsável pela atribuição do valor, nele é chamado o método validar que verifica se o valor está
+     * no formato correto, se estiver será atribuido, caso contrário será retornado throw.
+     *
+     * @param entrada string que será validada para atribuição.
+     * @throw invalid_argument caso não esteja no formato desejado.
+     *
+     * @see validar()
+     */
+    void setValor(const string &entrada) {
+        validar(entrada);
+        this->valor = entrada;
+    }
+
+    /** @brief Método Get
+     *
+     * Método responsável por retornar o valor
+     *
+     * @return valor
+     */
+    const string &getValor() const {
+        return valor;
+    }
+};
+
+#endif // DOMAIN_INTERFACE_H
