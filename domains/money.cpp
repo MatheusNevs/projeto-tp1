087890f91f9812b38e3da42f02ac24c2b28f535a
Artur Kohara Guerra <guerraartur1@gmail.com>
2024-10-29 11:08:29 -0300

    refactor: Domínio Money modificado

--- domains/money.cpp
+++ domains/money.cpp
@@ -1,21 +1,12 @@
 #include "money.hpp"
 using namespace std;
 
-bool Money::validate(float value)
+bool Money::validate(string value)
 {
-    if (value < 0 || value > 200000)
+    float quantity = stof(value);
+    if (quantity < 0 || quantity > 200000)
     {
         return false;
     }
     return true;
-}
-
-bool Money::setValue(float value)
-{
-    if (validate(value) == true)
-    {
-        this->value = value;
-        return true;
-    }
-    return false;
-}
+};
--- domains/money.hpp
+++ domains/money.hpp
@@ -7,23 +7,15 @@ using namespace std;
 class Money : public Domain
 {
 private:
-    float value;
-
     float moneyMax = 200000.00;
 
     float moneyMin = 0.00;
 
-    bool validate(float);
+    bool validate(string) override;
 
 public:
-    bool setValue(float);
-
-    float getValue() const;
+    Money(string value);
 };
 
-inline float Money::getValue() const
-{
-    return value;
-}
-
-#endif // DOMAIN_INTERFACE_H
+inline Money::Money(string value) : Domain(value) {}
+#endif // MONEY_INTERFACE_H
