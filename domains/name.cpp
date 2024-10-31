f15922cf4986fa9e5c1c52fa82388b54808060ea
felipe gibin <gibinf.duarte@gmail.com>
2024-10-31 19:59:09 -0300

    feat: documentação javadoc para validate (name)

--- domains/name.cpp
+++ domains/name.cpp
@@ -1,9 +1,18 @@
 #include "name.hpp"
 
+/**
+ * @brief Verifica se o nome fornecido é válido.
+ *
+ * Este método verifica se o comprimento do nome está dentro dos limites aceitáveis.
+ * Para ser válido, o nome deve ter entre 1 e 30 caracteres.
+ *
+ * @param name Nome que será verificado.
+ * @return true se o nome é válido (não vazio e com até 30 caracteres), false caso contrário.
+ */
 bool Name::validate(string name)
 {
     if (name.length() > 30 || name.empty())
-    { // name vazio ou maior que 30 caracteres
+    {
         return false;
     }
     return true;
