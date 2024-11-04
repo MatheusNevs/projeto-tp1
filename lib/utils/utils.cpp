84d35b9fad7c6aba6b6140e39aefbf912111b0de
MatheusNevs <matheusnf62@gmail.com>
2024-11-04 19:25:40 -0300

    feat: utils functions

---
+++ lib/utils/utils.cpp
@@ -0,0 +1,27 @@
+#include "utils.hpp"
+
+vector<string> Utils::split(const string &str, const char *delimiter)
+{
+  vector<string> tokens;
+  string token;
+  for (char ch : str)
+  {
+    if (&ch == delimiter)
+    {
+      if (!token.empty())
+      {
+        tokens.push_back(token);
+        token.clear();
+      }
+    }
+    else
+    {
+      token += ch;
+    }
+  }
+  if (!token.empty())
+  {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
