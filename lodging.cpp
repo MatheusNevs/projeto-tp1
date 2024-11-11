39f44da346d07f0ca3854f62b90af9a8f2ee5f16
felipe gibin <gibinf.duarte@gmail.com>
2024-11-11 11:31:38 -0300

    feat: cpp do lodging e formatacao

--- lodging.cpp
+++ lodging.cpp
@@ -1,1 +1,45 @@
 #include "lodging.hpp"
+
+// Metodo construtor
+Lodging::Lodging(const Code &code, const Name &name, const Money &dailyRate, const Rating &rating) : code(code), name(name), dailyRate(dailyRate), rating(rating) {}
+
+// get e set
+Code Lodging::getCode() const
+{
+    return code;
+}
+
+void Lodging::setCode(const Code &code)
+{
+    this -> code = code;
+}
+
+Name Lodging::getName() const
+{
+    return name;
+}
+
+void Lodging::setName(const Name &name)
+{
+    this -> name = name;
+}
+
+Money Lodging::getDailyRate() const
+{
+    return dailyRate;
+}
+
+void Lodging::setDailyRate(const Money &dailyRate)
+{
+    this -> dailyRate = dailyRate;
+}
+
+Rating Lodging::getRating() const
+{
+    return rating;
+}
+
+void Lodging::setRating(const Rating &rating)
+{
+    this -> rating = rating;
+}
--- lodging.hpp
+++ lodging.hpp
@@ -5,27 +5,28 @@
 
 using namespace std;
 
-class Lodging {
-    private:
-        Code code;
-        Name name;
-        Money dailyRate;
-        Rating rating;
+class Lodging
+{
+private:
+    Code code;
+    Name name;
+    Money dailyRate;
+    Rating rating;
 
-    public:
-        // Construtor
-        Lodging(const Code& code, const Name& name, const Money& dailyRate, const Rating& rating);
+public:
+    // Construtor
+    Lodging(const Code &code, const Name &name, const Money &dailyRate, const Rating &rating);
 
-        //Metodos get e set para cada atributo
-        Code getCode() const;
-        void setCode(const Code&); 
+    // Metodos get e set para cada atributo
+    Code getCode() const;
+    void setCode(const Code&);
 
-        Name getName() const;
-        void setName(const Name&);
+    Name getName() const;
+    void setName(const Name&);
 
-        Money getDailyRate() const;
-        void setDailyRate(const Money&);
+    Money getDailyRate() const;
+    void setDailyRate(const Money&);
 
-        Rating getRating() const;
-        void setRating(const Rating&);
+    Rating getRating() const;
+    void setRating(const Rating&);
 };
