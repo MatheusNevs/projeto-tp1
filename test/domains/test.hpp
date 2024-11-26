ef312bb6255c44fb7d76aee0984f66a2afd52a06
MatheusNevs <matheusnf62@gmail.com>
2024-11-26 11:34:08 -0300

    feat: javadoc and comments

--- test/domains/test.hpp
+++ test/domains/test.hpp
@@ -5,6 +5,11 @@
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 
+// Classe que recebe um domínio genérico, um valor válido para
+// este domínio e um valor inválido. Testa o setValue do dominio
+// recebido com valores validos e invalidos, esperando erro para
+// o valor invalido. Verifica se, caso não dê erro, o getValue
+// do domínio retorne o valor setado.
 class UnitTest
 {
 private:
@@ -20,6 +25,9 @@ public:
   std::string getDescription() const;
 };
 
+// Classe que receberá um nome e N UnitTest's que irá rodar os
+// testes dos respectivos testes de unidade mostrando no terminal
+// os resultados e, em caso de falha de teste, a mensagem da falha.
 class GroupTest
 {
 private:
