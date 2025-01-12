47077e80c8e302b5b24dc6acdd4f87a336bf11f8
MatheusNevs <matheusnf62@gmail.com>
2025-01-11 22:23:15 -0300

    feat: travel service interface and travel view hpp

--- include/interfaces/services.hpp
+++ include/interfaces/services.hpp
@@ -1,47 +1,22 @@
-#include "../entities/account.hpp"
+#include "../interfaces/presentation.hpp"
+#include "../domains/code.hpp"
+#include "../domains/name.hpp"
+#include "../domains/rating.hpp"
 #include "../entities/travel.hpp"
 #include "../entities/destination.hpp"
-#include "../entities/activity.hpp"
 #include "../entities/lodging.hpp"
+#include "../entities/activity.hpp"
 
-class ISAutenticacao
+class TravelServiceInterface
 {
 public:
-  virtual bool authenticate(Account account) = 0;
-};
-
-class ISTravel
-{
-public:
-  virtual void create(Travel travel) = 0;
-  virtual Travel read(int id) = 0;
-  virtual void update(Travel travel) = 0;
-  virtual void remove(int id) = 0;
-};
-
-class ISDestination
-{
-public:
-  virtual void create(Destination destination) = 0;
-  virtual Destination read(int id) = 0;
-  virtual void update(Destination destination) = 0;
-  virtual void remove(int id) = 0;
-};
-
-class ISActivity
-{
-public:
-  virtual void create(Activity activity) = 0;
-  virtual Activity read(int id) = 0;
-  virtual void update(Activity activity) = 0;
-  virtual void remove(int id) = 0;
-};
-
-class ISLodging
-{
-public:
-  virtual void create(Lodging lodging) = 0;
-  virtual Lodging read(int id) = 0;
-  virtual void update(Lodging lodging) = 0;
-  virtual void remove(int id) = 0;
+  virtual void create(Code userCode, Code travelCode, Name name) = 0;
+  virtual void update(Code travelCode) = 0;
+  virtual void remove(Code travelCode) = 0;
+  virtual vector<Travel> readAll(Code userCode) = 0;
+  virtual int consultCost(Code travelCode) = 0;
+  virtual vector<Destination> listDetinations(Code travelCode) = 0;
+  virtual Destination consultDestination(Code destinationCode) = 0;
+  virtual vector<Lodging> listLodgings(Code destinationCode) = 0;
+  virtual vector<Activity> listActivities(Code destinationCode) = 0;
 };
---
+++ include/views/travel.hpp
@@ -0,0 +1,24 @@
+#include "../interfaces/presentation.hpp"
+#include "../interfaces/services.hpp"
+#include "../domains/code.hpp"
+#include "../domains/name.hpp"
+#include "../domains/rating.hpp"
+
+class TravelView : PresentationInterface
+{
+private:
+  TravelServiceInterface *travelService;
+  Code userCode;
+  void create(Code travelCode, Name name);
+  void update(Code travelCode, Name name, Rating rating);
+  void remove(Code travelCode);
+  void listAll();
+  void consultTravel(Code travelCode);
+
+public:
+  void setTravelService(TravelServiceInterface *travelService)
+  {
+    travelService = travelService;
+  }
+  void executar(Code userCode) override;
+};
