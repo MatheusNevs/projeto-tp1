2178333f76a8dfef9fa822efb8971dea108f4e11
MatheusNevs <matheusnf62@gmail.com>
2025-01-11 22:22:13 -0300

    fix: presentation interface

--- include/interfaces/presentation.hpp
+++ include/interfaces/presentation.hpp
@@ -1,71 +1,7 @@
-#include "services.hpp"
+#include "../domains/code.hpp"
 
-class IAAuthPresentation
+class PresentationInterface
 {
-private:
-  ISAutenticacao *service;
-
 public:
-  virtual void setService(ISAutenticacao *service)
-  {
-    this->service = service;
-  }
-
-  virtual void present() = 0;
-};
-
-class IATravelPresentation
-{
-private:
-  ISTravel *service;
-
-public:
-  virtual void setService(ISTravel *service)
-  {
-    this->service = service;
-  }
-
-  virtual void present() = 0;
-};
-
-class IADestinationPresentation
-{
-private:
-  ISDestination *service;
-
-public:
-  virtual void setService(ISDestination *service)
-  {
-    this->service = service;
-  }
-
-  virtual void present() = 0;
-};
-
-class IAActivityPresentation
-{
-private:
-  ISActivity *service;
-
-public:
-  virtual void setService(ISActivity *service)
-  {
-    this->service = service;
-  }
-
-  virtual void present() = 0;
-};
-
-class IALodgingPresentation
-{
-private:
-  ISLodging *service;
-
-public:
-  virtual void setService(ISLodging *service)
-  {
-    this->service = service;
-  }
-
-  virtual void present() = 0;
+  virtual void executar(Code userCode) = 0;
 };
