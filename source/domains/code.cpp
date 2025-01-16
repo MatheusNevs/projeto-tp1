612845c6c4dc02906bc1f7b9e5f050122cd7f4b8
MatheusNevs <matheusnf62@gmail.com>
2025-01-15 22:59:22 -0300

    feat: generate random code for empty code constructor

--- source/domains/code.cpp
+++ source/domains/code.cpp
@@ -1,4 +1,6 @@
 // Classe implementada pelo aluno com matrícula 231013672
+#include <cstdlib>
+#include <ctime>
 
 #include "../../include/domains/code.hpp"
 
@@ -19,3 +21,26 @@ void Code::validate(string value)
     }
   }
 };
+
+Code::Code()
+{
+  string randomCode = generateRandomCode();
+  setValue(randomCode);
+}
+
+string Code::generateRandomCode()
+{
+  const char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const int alphanumLength = sizeof(alphanum) - 1;
+
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<> dis(0, alphanumLength - 1);
+
+  string randomCode;
+  for (int i = 0; i < codeLength; i++)
+  {
+    randomCode += alphanum[dis(gen)];
+  }
+  return randomCode;
+}
