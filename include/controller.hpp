8c86e56f460d07565620d463f2f42e44d40133b5
MatheusNevs <matheusnf62@gmail.com>
2025-01-19 17:48:47 -0300

    update: leak of memory on base class destructors (I didn't know how to fix it so I did this)

--- include/controller.hpp
+++ include/controller.hpp
@@ -1,24 +1,20 @@
 #ifndef CONTROLLER_INTERFACE_H
 #define CONTROLLER_INTERFACE_H
 
-#include "./interfaces/presentation.hpp"
+#include "./views/auth.hpp"
+#include "./views/travel.hpp"
 
 class Controller
 {
 private:
-  PresentationInterface *authView;
-  PresentationInterface *travelView;
-  PresentationInterface *destinationView;
-  PresentationInterface *lodgingView;
-  PresentationInterface *activityView;
+  AuthView *authView;
+  TravelView *travelView;
 
 public:
-  Code *userCode;
-  void setAuthView(PresentationInterface *authView);
-  void setTravelView(PresentationInterface *travelView);
-  void setDestinationView(PresentationInterface *destinationView);
-  void setLodgingView(PresentationInterface *lodgingView);
-  void setActivityView(PresentationInterface *activityView);
+  Code userCode = Code("000000");
+  ~Controller();
+  void setAuthView(AuthView *&authView);
+  void setTravelView(TravelView *&travelView);
 
   void execute();
 };
